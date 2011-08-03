067b03ce3d6a335a7eb7deec182dd1054f1ed4d2
Michael Friis <friism@gmail.com>
2011-08-02 17:50:32 -0700

    Import project demonstrating session state use

---
+++ MemcachedSessionStateDemo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MemcachedSessionStateDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var history = (List<string>)Session["ServerHistory"] ?? new List<string>();
+			history.Add(string.Format("{0}", System.Environment.MachineName));
+			Session["ServerHistory"] = history;
+			return View(history);
+		}
+	}
+}
---
+++ MemcachedSessionStateDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MemcachedSessionStateDemo.MvcApplication" Language="C#" %>
